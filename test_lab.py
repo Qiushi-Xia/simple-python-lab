902e7a3964bb4db9abbc5833258f10e9c7d0ce8c
Qiushi Xia <xia.qiu@northeastern.edu>
2024-06-22 14:30:56 +0800

    Add automated grading for student submissions

--- test_lab.py
+++ test_lab.py
@@ -1,11 +1,19 @@
+# test_students.py
 import unittest
-from lab import add
+import importlib
+import os
 
-class TestLab(unittest.TestCase):
-    def test_add(self):
-        self.assertEqual(add(2, 3), 5)
-        self.assertEqual(add(-1, 1), 0)
-        self.assertEqual(add(0, 0), 0)
+class TestStudentLabs(unittest.TestCase):
+    def test_student_labs(self):
+        students = [filename[:-3] for filename in os.listdir('student_labs') if filename.endswith('.py')]
+
+        for student in students:
+            mod = importlib.import_module(f'student_labs.{student}')
+            add = getattr(mod, 'add', None)
+            
+            self.assertIsNotNone(add, f"add function does not exist in {student}'s file")
+            
+            self.assertEqual(add(2, 3), 5, f"{student}'s output is wrong")
 
 if __name__ == "__main__":
     unittest.main()
